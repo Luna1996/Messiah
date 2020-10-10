c6ab19e64d56086b953e75732156323989683827
crystalxian <dzx07522@163.com>
2020-10-10 18:08:07 +0800

    add

--- Assets/0. Code/1. Logic/GameManager.cs
+++ Assets/0. Code/1. Logic/GameManager.cs
@@ -35,10 +35,10 @@ namespace Messiah.Logic {
     public static CardOnFly cardOnFly;
 
     public static int[] turneventpool = new int[]{
-      1,2
+      0,1,2
     };
     public static string[][] eventpool = new string[][] {
-      new string[]{"Event999"},new string[]{"Event1001"},
+      new string[]{"Event998"},new string[]{"Event999"},new string[]{"Event1001"},
     };
    
 
